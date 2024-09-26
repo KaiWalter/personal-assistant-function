9d436aaa2989d0af7400435822d636fabf985a6f
Kai Walter <kai.walter@zeiss.com>
2024-09-26 09:27:38 +0200

    don't count Freitag ab eins...

--- checkCalendar/index.js
+++ checkCalendar/index.js
@@ -1,128 +1,137 @@
 // expects a list of events in the body coming from Microsoft Graph me/calendar/{calendarId}/events
-module.exports = function(context, req) {
-    const threshold = parseInt(process.env.THRESHOLD) || 360; // 6 hours
-    const markerSubject = 'AUTO-BLOCKER';
+module.exports = function (context, req) {
+  const threshold = parseInt(process.env.THRESHOLD) || 360; // 6 hours
+  const markerSubject = "AUTO-BLOCKER";
 
-    // explode events spanning multiple days
-    let events = explodeMultipleDays(req);
+  // explode events spanning multiple days
+  let events = explodeMultipleDays(req);
 
-    // go through each event and total duration
-    let totals = calculateTotals(events, markerSubject);
+  // go through each event and total duration
+  let totals = calculateTotals(events, markerSubject);
 
-    // determine when to create a blocker and when to remove it
-    let creates = [];
-    let deletes = [];
+  // determine when to create a blocker and when to remove it
+  let creates = [];
+  let deletes = [];
 
-    totals.forEach(e => {
-        if (e.isBlocked) {
-            if (e.total < threshold || e.totalBlocks > 1) { // delete block when below threshold or when redundant blocks
-                if (e.id) {
-                    deletes.push({ day: e.day, id: e.id });
-                }
-            }
-        } else {
-            if (e.total > threshold) { // create block when above threshold
-                creates.push({ day: e.day, event: markerSubject });
-            }
+  totals.forEach((e) => {
+    if (e.isBlocked) {
+      if (e.total < threshold || e.totalBlocks > 1) {
+        // delete block when below threshold or when redundant blocks
+        if (e.id) {
+          deletes.push({ day: e.day, id: e.id });
         }
-    });
+      }
+    } else {
+      if (e.total > threshold) {
+        // create block when above threshold
+        creates.push({ day: e.day, event: markerSubject });
+      }
+    }
+  });
 
-    context.res = {
-        body: {
-            creates: creates,
-            deletes: deletes,
-            totals: totals
-        }
-    };
+  context.res = {
+    body: {
+      creates: creates,
+      deletes: deletes,
+      totals: totals,
+    },
+  };
 
-    context.done();
+  context.done();
 };
 
-Date.prototype.addDays = function(days) {
-    const date = new Date(this.valueOf());
-    date.setDate(date.getDate() + days);
-    return date;
+Date.prototype.addDays = function (days) {
+  const date = new Date(this.valueOf());
+  date.setDate(date.getDate() + days);
+  return date;
 };
 
 function explodeMultipleDays(req) {
-    let events = [];
-    req.body.forEach(e => {
-        if (e.isAllDay && e.start.substring(0, 10) < e.end.substring(0, 10)) {
-            var start = new Date(e.start);
-            var end = new Date(e.end);
-            var day = start;
-            while (day < end) {
+  let events = [];
+  req.body.forEach((e) => {
+    if (e.isAllDay && e.start.substring(0, 10) < e.end.substring(0, 10)) {
+      var start = new Date(e.start);
+      var end = new Date(e.end);
+      var day = start;
+      while (day < end) {
+        var eClone = Object.assign({}, e);
 
-                var eClone = Object.assign({}, e);;
+        eClone.start = day.toISOString();
+        eClone.end = day.toISOString();
+        events.push(eClone);
 
-                eClone.start = day.toISOString();
-                eClone.end = day.toISOString();
-                events.push(eClone);
-
-                day = day.addDays(1);
-            }
-        } else {
-            events.push(e);
-        }
-    });
-    return events;
+        day = day.addDays(1);
+      }
+    } else {
+      events.push(e);
+    }
+  });
+  return events;
 }
 
 function calculateTotals(events, markerSubject) {
-    let totals = [];
+  let totals = [];
 
-    events.forEach(e => {
-        var start = new Date(e.start);
-        var end = new Date(e.end);
+  events.forEach((e) => {
+    var start = new Date(e.start);
+    var end = new Date(e.end);
 
-        if (start.getDay() > 0 && start.getDay() < 6) { // only count weekdays
-            // create aggregation entry
-            var day = start.toISOString().substring(0, 10);
-            var entry = totals.find(e => e.day === day);
-            if (!entry) {
-                totals.push({
-                    day: day,
-                    total: 0,
-                    totalBlocks: 0,
-                    isBlocked: false,
-                    slots: new Array(24 * 4).fill(0) // 4 slots per hour
-                });
-                entry = totals.find(e => e.day === day);
-            }
+    if (start.getDay() > 0 && start.getDay() < 6) {
+      // only count weekdays
+      // create aggregation entry
+      var day = start.toISOString().substring(0, 10);
+      var entry = totals.find((e) => e.day === day);
+      if (!entry) {
+        totals.push({
+          day: day,
+          total: 0,
+          totalBlocks: 0,
+          isBlocked: false,
+          slots: new Array(24 * 4).fill(0), // 4 slots per hour
+        });
+        entry = totals.find((e) => e.day === day);
+      }
 
-            // capture already blocked days
-            if (entry) {
-                if (e.isAllDay && e.subject === markerSubject) {
-                    entry.isBlocked = true;
-                    entry.totalBlocks++;
-                    entry.id = e.id;
-                } else if (e.isAllDay && e.showAs !== 'free') { // count complete blocks
-                    entry.isBlocked = true;
-                    entry.totalBlocks++;
-                } else if (!e.isAllDay && e.showAs !== 'free' && e.sensitivity !== 'private' ) { // only count busy days and non private meetings
-                    calculateTotalMinutes(start, end, entry);
-                }
-            }
+      // capture already blocked days
+      if (entry) {
+        if (e.isAllDay && e.subject === markerSubject) {
+          entry.isBlocked = true;
+          entry.totalBlocks++;
+          entry.id = e.id;
+        } else if (e.isAllDay && e.showAs !== "free") {
+          // count complete blocks
+          entry.isBlocked = true;
+          entry.totalBlocks++;
+        } else if (
+          !e.isAllDay &&
+          e.showAs !== "free" &&
+          e.sensitivity !== "private" &&
+          e.subject !== "Freitag ab Eins..."
+        ) {
+          // only count busy days and non private meetings
+          calculateTotalMinutes(start, end, entry);
         }
+      }
+    }
+  });
 
-    });
-
-    return totals;
+  return totals;
 }
 
 function calculateTotalMinutes(start, end, entry) {
+  // fill slots
+  var startSlot =
+    start.getHours() * 4 + Math.floor(start.getMinutes() / 15) - 1;
+  var endSlot = end.getHours() * 4 + Math.floor(end.getMinutes() / 15) - 1;
+  if (end.getMinutes() % 15 !== 0) {
+    // fill to 15 minute slot
+    endSlot++;
+  }
 
-    // fill slots
-    var startSlot = (start.getHours() * 4 + Math.floor(start.getMinutes() / 15)) - 1;
-    var endSlot = (end.getHours() * 4 + Math.floor(end.getMinutes() / 15)) - 1;
-    if (end.getMinutes() % 15 !== 0) { // fill to 15 minute slot
-        endSlot++;
-    }
+  for (var i = startSlot; i < endSlot; i++) {
+    entry.slots[i] = 1;
+  }
 
-    for (var i = startSlot; i < endSlot; i++) {
-        entry.slots[i] = 1;
-    }
-
-    // count slots with value 1 and convert to minutes
-    entry.total = entry.slots.reduce((a, b) => a + b, 0) * 15;
+  // count slots with value 1 and convert to minutes
+  entry.total = entry.slots.reduce((a, b) => a + b, 0) * 15;
 }
